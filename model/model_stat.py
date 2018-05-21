2675cbb2fd01c9a4971001df4e24dbe890fb72b3
iam91 <zhengwy91@foxmail.com>
2018-05-21 20:57:52 +0800

    update

--- model/model_stat.py
+++ model/model_stat.py
@@ -1,28 +1,26 @@
 import pandas as pd 
 
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score
-from sklearn.metrics import recall_score
 
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 from cv import CV
 
 SEED = 2018
-N_SPLITS = 3
+NSPLITS = 3
 FEATURE_COL = 'url_len url_path_len url_path_str url_query_cnt url_query_str'.split()
 
 if __name__ == '__main__':
 
     df = pd.read_csv('../data_stat.csv')    
+
+    # data scaling
     scaler = MinMaxScaler(copy=False)
-    kfold = StratifiedKFold(n_splits=N_SPLITS, shuffle=True, random_state=SEED)
 
     df[FEATURE_COL] = scaler.fit_transform(df[FEATURE_COL].values)
     df.drop(['site'], axis=1, inplace=True)
-
     df.sample(frac=1, random_state=SEED).reset_index(inplace=True)
 
     dfy = df['target']
@@ -32,10 +30,11 @@ if __name__ == '__main__':
     print dfx.values.shape
     print '-' * 20
 
-    cv = CV(dfx, dfy, N_SPLITS, SEED)
+    cv = CV(dfx, dfy, NSPLITS, SEED)
 
     # model
+    clf = LogisticRegression(C=0.01)
     clf = RandomForestClassifier(n_estimators=10)
-    # clf = SVC(kernel='rbf', random_state=SEED, C=50.0)
+    clf = SVC(kernel='rbf', random_state=SEED, C=10.0)
 
     cv.validate(clf)
